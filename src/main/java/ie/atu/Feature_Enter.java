91f6837e1107d76ce30eeed3cbeb8c4d0259d211
Flippso91 <g00351833@atu.ie>
2022-10-17 11:53:14 +0100

    request to find width and print...

--- src/main/java/ie/atu/Feature_Enter.java
+++ src/main/java/ie/atu/Feature_Enter.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Feature_Enter {
     public static void main(String[] args) {
-        System.out.println("Please enter length");
+
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter length");
         double length = input.nextDouble();
         System.out.println("You entered: " + length);
 
+        System.out.println("Please enter width");
+        double width = input.nextDouble();
+        System.out.println("You entered: " + width);
+        System.out.println("Length x Width = " + length + " x " + width);
     }
 }
