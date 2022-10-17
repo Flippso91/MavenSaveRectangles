9a5c95b490f1c5235b997f4ddbebca5eca12e92f
Flippso91 <g00351833@atu.ie>
2022-10-17 11:45:44 +0100

    request to find length

--- src/main/java/ie/atu/Feature_Enter.java
+++ src/main/java/ie/atu/Feature_Enter.java
@@ -1,4 +1,13 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Feature_Enter {
+    public static void main(String[] args) {
+        System.out.println("Please enter length");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+        System.out.println("You entered: " + length);
+
+    }
 }
