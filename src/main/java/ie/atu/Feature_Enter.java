0abcfd30e807631ee99e6872949d94a663d6e0ee
Flippso91 <g00351833@atu.ie>
2022-10-24 16:52:31 +0100

    used code to input length and width into new rectangle class...

--- src/main/java/ie/atu/Feature_Enter.java
+++ src/main/java/ie/atu/Feature_Enter.java
@@ -9,11 +9,14 @@ public class Feature_Enter {
 
         System.out.println("Please enter length");
         double length = input.nextDouble();
+        Rectangle shapeRect = new Rectangle();
         System.out.println("You entered: " + length);
+        shapeRect.setLength(length);
 
         System.out.println("Please enter width");
         double width = input.nextDouble();
         System.out.println("You entered: " + width);
-        System.out.println("Length x Width = " + length + " x " + width);
+        shapeRect.setLength(width);
+        System.out.println("Length x Width = " + length + " x " + width + " = " + length * width);
     }
 }
